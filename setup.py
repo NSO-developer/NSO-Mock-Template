c311ea87cac61ad576ffa10422a9a4c210140602
Brandon Black <bblackifyme@gmail.com>
2018-02-05 14:10:03 -0800

    Updated version # for .raw change
    Signed-off-by: Brandon Black <bblackifyme@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MonkeyTemplate',
-    version='0.1.1',
+    version='0.1.2',
     author='Brandon Black',
     author_email='branblac@cisco.com',
     description='Mock ncs.application.Template tool for NSO testing',
